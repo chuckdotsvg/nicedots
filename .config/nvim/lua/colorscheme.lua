537a7db4f4c7d34552965105017d1b8b7de3ac6c
Carlo Aspren Gines Lara <101257700+PinoTheCreator@users.noreply.github.com>
2022-10-25 02:04:22 +0200

    routine save

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "gruvbox"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
--- .local/bin/startway.sh
+++ .local/bin/startway.sh
@@ -2,23 +2,27 @@
 
 #systemctl --user import-environment XDG_CURRENT_DESKTOP WAYLAND_DISPLAY
 
-# killall pipewire; killall pipewire-pulse; killall wireplumber; pipewire & pipewire & pipewire-pulse & wireplumber &
+# audio
+pipewire &
+pipewire-pulse &
+wireplumber &
+playerctld daemon &
+
+# notifications
+dunst &
 
 # status bar
 waybar &
-# eww daemon &
-# eww open bar &
-
-swaybg -i ~/Pictures/forest-hut.png &
-dunst &
 udiskie &
+blueman-applet &
+nm-applet &
+
+# graphic stuff
+swaybg -i ~/Pictures/forest-hut.png &
 swayidle -w &
-wlsunset -l 41.9 -L 12.5 &
-playerctld daemon &
+gammastep &
 
 # dbus-update-activation-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 # killall swhks; swhks &
 # pkexec swhkd -c ~/.config/swhkd/swhkdrc
-
-# exec <&-
